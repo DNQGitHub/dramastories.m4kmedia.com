d23f11b9528c334859f13046dc4489b5a63a5e19
quang-doan-tpv-clv <dnq.httt@gmail.com>
2026-07-22 01:00:46 +0700

    chore: force dynamic serrver component

--- src/app/articles/[articleSlug]/page.tsx
+++ src/app/articles/[articleSlug]/page.tsx
@@ -1,3 +1,5 @@
 import { Page as DramaStoriesArticleDetailPage } from "@/modules/drama-stories/pages/article-detail/Page";
 
+export const dynamic = "force-dynamic";
+
 export default DramaStoriesArticleDetailPage;
--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,3 +1,5 @@
 import { Page as DramaStoriesArticleListPage } from "@/modules/drama-stories/pages/article-list/Page";
 
+export const dynamic = "force-dynamic";
+
 export default DramaStoriesArticleListPage;
